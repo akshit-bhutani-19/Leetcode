ef76e7b5202b4c100560b37832657aff230e9371
Akki019 <Akshit.105013@stu.upes.ac.in>
2025-06-28 20:58:03 +0530

    BubbleSort

--- Leetcode_Questions/Algorithms/Sorting/BubbleSort.cpp
+++ Leetcode_Questions/Algorithms/Sorting/BubbleSort.cpp
@@ -3,29 +3,39 @@
 
 using namespace std;
 
-vector<int> BubbleSort(vector<int> v){
-    int temp;
-    int n= v.size();
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(v[j]>v[j+1]){
-                temp=v[j];
-                v[j]=v[j+1];
-                v[j+1]=temp;
-            }
-        }
-    }
-    return v;
-}
-
 void PrintVector(vector<int> v){
     for (auto it:v){
         cout << it << " ";
     }
 }
 
+vector<int> BubbleSort(vector<int> v){
+    int temp;
+    int n= v.size();
+    bool notSwap= true;
+    for(int i=0;i<n-1;i++){
+        notSwap=true;
+        for(int j=0;j<n-i-1;j++){
+            if(v[j]>v[j+1]){
+                temp=v[j];
+                v[j]=v[j+1];
+                v[j+1]=temp;
+                notSwap=false;
+            }
+            PrintVector(v);
+            cout << endl;
+        }
+        if (notSwap){
+            break;
+        }
+        cout << endl;
+    }
+    return v;
+}
+
+
 int main(){
-    vector<int> v = {2,4,7,1,23,54,2,34,76,4,34,1,34,65,63};
+    vector<int> v = {23,54,2,34,76,4,34,1,34,65,63,12,32,1,99,100,101};
     vector<int> v2 = BubbleSort(v);
     PrintVector(v2);
 }

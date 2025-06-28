40b462b74994572d2eb75515be0881fb47d0e331
Akki019 <Akshit.105013@stu.upes.ac.in>
2025-06-28 19:34:15 +0530

    SelectionSort

---
+++ Leetcode_Questions/Algorithms/Sorting/SelectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> SelectionSort(vector<int> v){
+    int pointer, min;
+    int temp;
+    int n=v.size();
+    for(int i=0; i<n; i++){
+        min=99990;
+        for(int j=i;j<n;j++){
+            if(v[j]<min){
+                min = v[j];
+                pointer=j; 
+            }
+        }
+        temp=v[i];
+        v[i]=v[pointer];
+        v[pointer]=temp;
+    }
+    return v;
+}
+
+void PrintVector(vector<int> v){
+    for (auto it:v){
+        cout << it << " ";
+    }
+}
+
+int main(){
+    vector<int> v = {2,4,7,1,23,54,2,34,76,4,34,1,34,65,63};
+    vector<int> v2 = SelectionSort(v);
+    PrintVector(v2);
+}
